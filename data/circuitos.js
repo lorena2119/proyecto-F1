33f6223f2d03ae185b85c8900fe2b8b03cd15c20
DavidFG16 <189179517+DavidFG16@users.noreply.github.com>
2025-04-30 09:02:35 -0500

    feat: :sparkles: Web Component de las tarjetas de circuitos y su función de visualización

--- data/circuitos.js
+++ data/circuitos.js
@@ -1,103 +1,107 @@
-const circuitos = [
+export const circuitos = [
     {
-    nombre: "Circuito de Mónaco",
-    pais: "Mónaco",
-    longitud_km: 3.34,
-    vueltas: 78,
-    descripcion: "Uno de los circuitos más prestigiosos y difíciles del calendario, conocido por sus calles angostas y la falta de zonas de adelantamiento.",
-    record_vuelta: { tiempo: "1:10.166", piloto: "Lewis Hamilton", año: 2019 },
-    ganadores: [
-    { temporada: 2021, piloto: 1 },
-    { temporada: 2022, piloto: 2 },
-    { temporada: 2023, piloto: 1 }
-    ],
-    imagen: "https://img.redbull.com/images/q_auto,f_auto/redbullcom/2023/4/6/qf1iayummv79s4tdzkyy/plano-circuito-monaco-formula-1"
-},
-{
-    nombre: "Silverstone",
-    pais: "Reino Unido",
-    longitud_km: 5.89,
-    vueltas: 52,
-    descripcion: "Uno de los circuitos más rápidos del calendario, con curvas de alta velocidad como Maggotts y Becketts.",
-    record_vuelta: { tiempo: "1:27.097", piloto: "Max Verstappen", año: 2020 },
-    ganadores: [
-    { temporada: 2021, piloto: 3 },
-    { temporada: 2022, piloto: 5 },
-    { temporada: 2023, piloto: 1 }
-    ],
-    imagen: "https://upload.wikimedia.org/wikipedia/commons/c/ca/Circuit_Silverstone_2011.svg"
-},
-{
-    nombre: "Circuito de Spa-Francorchamps",
-    pais: "Bélgica",
-    longitud_km: 7.00,
-    vueltas: 44,
-    descripcion: "Famoso por la curva Eau Rouge y la larga recta de Kemmel, un circuito donde la potencia del motor es clave.",
-    record_vuelta: { tiempo: "1:46.286", piloto: "Valtteri Bottas", año: 2018 },
-    ganadores: [
-    { temporada: 2021, piloto: 1 },
-    { temporada: 2022, piloto: 1 },
-    { temporada: 2023, piloto: 1 }
-    ],
-    imagen: "https://www.motoryracing.com/images/circuitos/0000/24.png"
-},
-{
-    nombre: "Circuito de Monza",
-    pais: "Italia",
-    longitud_km: 5.79,
-    vueltas: 53,
-    descripcion: "Conocido como 'El Templo de la Velocidad', Monza es el circuito más rápido del calendario con largas rectas y chicanes icónicas.",
-    record_vuelta: { tiempo: "1:21.046", piloto: "Rubens Barrichello", año: 2004 },
-    ganadores: [
-    { temporada: 2021, piloto: 2 },
-    { temporada: 2022, piloto: 1 },
-    { temporada: 2023, piloto: 1 }
-    ],
-    imagen: "https://upload.wikimedia.org/wikipedia/commons/a/ae/Monza_1976-1993.png"
-},
-{
-    nombre: "Interlagos",
-    pais: "Brasil",
-    longitud_km: 4.31,
-    vueltas: 71,
-    descripcion: "Interlagos es un circuito legendario con cambios de elevación y un trazado técnico que ha sido sede de algunas de las carreras más emocionantes de la historia.",
-    record_vuelta: { tiempo: "1:10.540", piloto: "Valtteri Bottas", año: 2018 },
-    ganadores: [
-    { temporada: 2021, piloto: 3 },
-    { temporada: 2022, piloto: 1 },
-    { temporada: 2023, piloto: 1 }
-    ],
-    imagen: "https://www.racingatmosphere.net/wp-content/uploads/2018/08/Circuit_Interlagos.svg_.png"
-},
-{
-    nombre: "Circuito de Yas Marina",
-    pais: "Emiratos Árabes Unidos",
-    longitud_km: 5.28,
-    vueltas: 58,
-    descripcion: "Ubicado en Abu Dhabi, es famoso por ser el circuito donde se definen muchos campeonatos, con un diseño moderno y una espectacular carrera nocturna.",
-    record_vuelta: { tiempo: "1:39.283", piloto: "Lewis Hamilton", año: 2019 },
-    ganadores: [
-    { temporada: 2021, piloto: 1 },
-    { temporada: 2022, piloto: 1 },
-    { temporada: 2023, piloto: 3 }
-    ],
-    imagen: "https://static.wikia.nocookie.net/f1wikia/images/d/dc/Circuit_Yas-Island.svg/revision/latest?cb=20120806124014"
-},
-
-{
-    nombre: "Circuito de Suzuka",
-    pais: "Japón",
-    longitud_km: 5.81,
-    vueltas: 53,
-    descripcion: "Un circuito desafiante con un diseño en forma de ocho, famoso por sus curvas de alta velocidad como 130R y la 'S' de Senna.",
-    record_vuelta: { tiempo: "1:30.983", piloto: "Lewis Hamilton", año: 2019 },
-    ganadores: [
-    { temporada: 2021, piloto: 1 },
-    { temporada: 2022, piloto: 1 },
-    { temporada: 2023, piloto: 1 }
-    ],
-
-    imagen: "https://upload.wikimedia.org/wikipedia/commons/e/ec/Suzuka_circuit_map--2005.svg"
-}
-
-];
+      nombre: "Circuito de Mónaco",
+      pais: "Mónaco",
+      longitud_km: 3.34,
+      vueltas: 78,
+      descripcion: "Uno de los circuitos más prestigiosos y difíciles del calendario, conocido por sus calles angostas y la falta de zonas de adelantamiento.",
+      condiciones_pista: "Urbano, superficie resbaladiza",
+      record_vuelta: { tiempo: "1:10.166", piloto: "Lewis Hamilton", año: 2019 },
+      ganadores: [
+        { temporada: 2021, piloto: 1 },
+        { temporada: 2022, piloto: 2 },
+        { temporada: 2023, piloto: 1 }
+      ],
+      imagen: "https://img.redbull.com/images/q_auto,f_auto/redbullcom/2023/4/6/qf1iayummv79s4tdzkyy/plano-circuito-monaco-formula-1"
+    },
+    {
+      nombre: "Silverstone",
+      pais: "Reino Unido",
+      longitud_km: 5.89,
+      vueltas: 52,
+      descripcion: "Uno de los circuitos más rápidos del calendario, con curvas de alta velocidad como Maggotts y Becketts.",
+      condiciones_pista: "Asfalto, alta adherencia",
+      record_vuelta: { tiempo: "1:27.097", piloto: "Max Verstappen", año: 2020 },
+      ganadores: [
+        { temporada: 2021, piloto: 3 },
+        { temporada: 2022, piloto: 5 },
+        { temporada: 2023, piloto: 1 }
+      ],
+      imagen: "https://upload.wikimedia.org/wikipedia/commons/c/ca/Circuit_Silverstone_2011.svg"
+    },
+    {
+      nombre: "Circuito de Spa-Francorchamps",
+      pais: "Bélgica",
+      longitud_km: 7.00,
+      vueltas: 44,
+      descripcion: "Famoso por la curva Eau Rouge y la larga recta de Kemmel, un circuito donde la potencia del motor es clave.",
+      condiciones_pista: "Mixto, propenso a lluvia",
+      record_vuelta: { tiempo: "1:46.286", piloto: "Valtteri Bottas", año: 2018 },
+      ganadores: [
+        { temporada: 2021, piloto: 1 },
+        { temporada: 2022, piloto: 1 },
+        { temporada: 2023, piloto: 1 }
+      ],
+      imagen: "https://www.motoryracing.com/images/circuitos/0000/24.png"
+    },
+    {
+      nombre: "Circuito de Monza",
+      pais: "Italia",
+      longitud_km: 5.79,
+      vueltas: 53,
+      descripcion: "Conocido como 'El Templo de la Velocidad', Monza es el circuito más rápido del calendario con largas rectas y chicanes icónicas.",
+      condiciones_pista: "Asfalto rápido, baja carga aerodinámica",
+      record_vuelta: { tiempo: "1:21.046", piloto: "Rubens Barrichello", año: 2004 },
+      ganadores: [
+        { temporada: 2021, piloto: 2 },
+        { temporada: 2022, piloto: 1 },
+        { temporada: 2023, piloto: 1 }
+      ],
+      imagen: "https://upload.wikimedia.org/wikipedia/commons/a/ae/Monza_1976-1993.png"
+    },
+    {
+      nombre: "Interlagos",
+      pais: "Brasil",
+      longitud_km: 4.31,
+      vueltas: 71,
+      descripcion: "Interlagos es un circuito legendario con cambios de elevación y un trazado técnico que ha sido sede de algunas de las carreras más emocionantes de la historia.",
+      condiciones_pista: "Asfalto técnico, cambios de altitud",
+      record_vuelta: { tiempo: "1:10.540", piloto: "Valtteri Bottas", año: 2018 },
+      ganadores: [
+        { temporada: 2021, piloto: 3 },
+        { temporada: 2022, piloto: 1 },
+        { temporada: 2023, piloto: 1 }
+      ],
+      imagen: "https://www.racingatmosphere.net/wp-content/uploads/2018/08/Circuit_Interlagos.svg_.png"
+    },
+    {
+      nombre: "Circuito de Yas Marina",
+      pais: "Emiratos Árabes Unidos",
+      longitud_km: 5.28,
+      vueltas: 58,
+      descripcion: "Ubicado en Abu Dhabi, es famoso por ser el circuito donde se definen muchos campeonatos, con un diseño moderno y una espectacular carrera nocturna.",
+      condiciones_pista: "Moderno, buena visibilidad nocturna",
+      record_vuelta: { tiempo: "1:39.283", piloto: "Lewis Hamilton", año: 2019 },
+      ganadores: [
+        { temporada: 2021, piloto: 1 },
+        { temporada: 2022, piloto: 1 },
+        { temporada: 2023, piloto: 3 }
+      ],
+      imagen: "https://upload.wikimedia.org/wikipedia/commons/d/dc/Circuit_Yas-Island.svg"
+    },
+    {
+      nombre: "Circuito de Suzuka",
+      pais: "Japón",
+      longitud_km: 5.81,
+      vueltas: 53,
+      descripcion: "Un circuito desafiante con un diseño en forma de ocho, famoso por sus curvas de alta velocidad como 130R y la 'S' de Senna.",
+      condiciones_pista: "Técnico, superficie con buena adherencia",
+      record_vuelta: { tiempo: "1:30.983", piloto: "Lewis Hamilton", año: 2019 },
+      ganadores: [
+        { temporada: 2021, piloto: 1 },
+        { temporada: 2022, piloto: 1 },
+        { temporada: 2023, piloto: 1 }
+      ],
+      imagen: "https://upload.wikimedia.org/wikipedia/commons/e/ec/Suzuka_circuit_map--2005.svg"
+    }
+  ];
