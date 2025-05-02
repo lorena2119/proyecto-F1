15f34e0f3967b7280bb9c5919bbf20f43bddfa14
DavidFG16 <189179517+DavidFG16@users.noreply.github.com>
2025-05-02 07:52:19 -0500

    feat: :zap: Mejorando desempeño al borrar tarjetas con el LocalStorage

--- main.js
+++ main.js
@@ -2,23 +2,9 @@ import './components/circuitos.js'
 import './components/pilotos.js'
 import './components/simulacion.js'
 import './components/vehiculos.js';
-import { vehiculos } from './data/vehiculos.js';
+import { vehiculos as vehiculosData } from "./data/vehiculos.js";
 
-const container = document.getElementById('vehiculos-container');
-
-vehiculos.forEach((vehiculo, index) => {
-  const card = document.createElement("vehiculo-card");
-  card.data = vehiculo;
-  card.dataset.id = index; // para identificarlo luego
-  container.appendChild(card);
-
-  localStorage.setItem('borrar-elemento',card.addEventListener("vehiculo-eliminado", (e) => {
-    const id = e.detail;
-    vehiculos.splice(id, 1); 
-    card.remove(); 
-  }));
-});
-
+let vehiculos = JSON.parse(localStorage.getItem("vehiculos_guardados")) || [...vehiculosData];
 document.addEventListener("DOMContentLoaded", () => {
   const icon = document.querySelector(".hamburger-icon");
 
@@ -29,4 +15,24 @@ document.addEventListener("DOMContentLoaded", () => {
       icon.classList.toggle("open");
     });
   }
-});
+})
+
+const container = document.getElementById('vehiculos-container');
+
+vehiculos.forEach((vehiculo, index) => {
+  const card = document.createElement("vehiculo-card");
+  card.data = vehiculo;
+  card.dataset.id = index;
+  container.appendChild(card);
+
+  card.addEventListener("vehiculo-eliminado", (e) => {
+    const id = e.detail;
+    vehiculos.splice(id, 1); 
+    card.remove(); 
+    localStorage.setItem("vehiculos_guardados", JSON.stringify(vehiculos));
+  })
+
+  let vehiculosGuardados = localStorage.getItem("vehiculos_guardados");
+  if (vehiculosGuardados) {
+    vehiculos = JSON.parse(vehiculosGuardados); 
+  }})
