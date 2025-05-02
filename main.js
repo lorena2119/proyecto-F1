eac281600fc0213de4b24731b884a967e5be0f59
DavidFG16 <189179517+DavidFG16@users.noreply.github.com>
2025-05-02 15:58:12 -0500

    fix: :bug: Arreglando la funcion de redireccionar al usuario ya que se ejecutaba antes del DOM

--- main.js
+++ main.js
@@ -5,25 +5,30 @@ import './components/vehiculos.js';
 import { vehiculos as vehiculosData } from "./data/vehiculos.js";
 
 // Función para redireccionar según el usuario: user o admin
-document.getElementById('form').addEventListener('submit', function(event) {
-  event.preventDefault(); 
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById('form');
+  if (form) {
+    form.addEventListener('submit', function(event) {
+      event.preventDefault(); 
 
-  let userUser = "user123";
-  let userAdmin = "admin123";
-  let passwordUser = "user123";
-  let passwordAdmin = "admin123";
+      let userUser = "user123";
+      let userAdmin = "admin123";
+      let passwordUser = "user123";
+      let passwordAdmin = "admin123";
 
-  let user = document.getElementById('user').value;
-  let password = document.getElementById('password').value;
+      let user = document.getElementById('user').value;
+      let password = document.getElementById('password').value;
 
-  if(user === userUser && password === passwordUser){
-      window.location.href = 'inicio.html';
-  } else if(user === userAdmin && password === passwordAdmin){
-      window.location.href = 'inicioAdmin.html';
-  } else {
-      document.getElementById('error').textContent = 'Contraseña o usuario incorrecto';
+      if(user === userUser && password === passwordUser){
+          window.location.href = 'inicio.html';
+      } else if(user === userAdmin && password === passwordAdmin){
+          window.location.href = 'inicioAdmin.html';
+      } else {
+          document.getElementById('error').textContent = 'Contraseña o usuario incorrecto';
+      }
+    });
   }
-  });
+});
 
 let vehiculos = JSON.parse(localStorage.getItem("vehiculos_guardados")) || [...vehiculosData];
 document.addEventListener("DOMContentLoaded", () => {
