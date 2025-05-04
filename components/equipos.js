4b2e1fb391a1e48dac2fa155eefaa24c5cef900d
lorena2119 <[189173816+lorena2119@users.noreply.github.com](mailto:189173816+lorena2119@users.noreply.github.com)>
2025-05-04 18:25:40 -0500

    feat: :construction: Agregar nuevo equipo

--- components/equipos.js
+++ components/equipos.js
@@ -339,10 +339,163 @@ class EquiposCardAdmin extends HTMLElement {
             font-size: 0.9rem;
             color: #aaa;
           }
+          .input-box{
+        position: relative;
+        width: 310px;
+        margin: 20px 0;
+        border-bottom: 2px solid #000000;
+      }
+
+      .input-box label{
+        position: absolute;
+        top:50%;
+        left: 5px;
+        transform: translateY(-50%);
+        font-size: 1rem;
+        color: #000000;
+        pointer-events: none;
+        transition: .5s;
+      }
+      .input-box input:focus~label,
+      .input-box input:valid~label{
+        top: -5px;
+      }
+      .input-box input{
+        width: 100%;
+        height: 40px;
+        background: transparent;
+        border: none;
+        outline: none;
+        font-size: 1rem;
+        color: #000000;
+        padding: 0 0px 0 5px;
+      }
+      .button{
+        width: 100%;
+        height: 40px;
+        background: rgb(225, 6, 0);
+        border: none;
+        outline: none;
+        border-radius: 40px;
+        cursor: pointer;
+        font-size: 1em;
+        color: #ffffff;
+        font-weight: 500;
+        font-family: "Bruno Ace SC";
+      }
+      .modal{
+        position: fixed;
+        bottom: 50%;
+        left: 50%;
+        transform: translate(-50%, 50%) scale(0);
+        transition: 200ms ease-in-out;
+        background-color: #ffffff;
+        border-radius: 10px;
+        z-index: 50000;
+        width: 500px;
+        max-width: 80%;
+        }
+      .modal.active{
+        transform: translate(-50%, 50%) scale(1);
+      }
+      .modal-header{
+        padding: 10px 15px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        border-bottom: 1px solid #e4dddd;
+      }
+      h2{
+        font-weight: bold;
+        font-size: 2rem;
+        color: #000000;
+        margin: 0;
+        text-align: center;
+      }
+
+      .modal-header .close-button{
+        cursor: pointer;
+        border: none;
+        outline: none;
+        background: none;
+        font-size: 1.2rem;
+        font-weight: bold;
+      }
+      .modal-body{
+        display: flex;
+        flex-direction: column; 
+        justify-content: center;
+        align-items: center;
+        padding: 10px 15px;
+
+      }
+      #overlay{
+        position: fixed;
+        opacity: 0;
+        transition: 200ms ease-in-out;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: 30000;
+        background-color:rgba(0, 0, 0, 0.54);
+        pointer-events: none
+      }
+        #overlay.active{
+          pointer-events: all;
+          opacity: 1;
+        }
             @media screen and (max-width: 1500px) {
-          .container {
-            grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));font-family: "Bruno Ace SC";
-            margin: 1rem 35px;
+        .container {
+          grid-template-columns: repeat(auto-fit, minmax(245px, 1fr));font-family: "Bruno Ace SC";
+          margin: 1rem 35px;
+          }
+      }
+      #buttonAdd{
+      padding: 10px 10px;
+      position: fixed; 
+      bottom: 5%;
+      font-family: "Bruno Ace SC";
+      right: 5%;
+      box-shadow: 0px 3px 20px rgba(255, 255, 255, 0.4);
+      background-color: rgb(0, 0, 0);
+      z-index: 20000;
+      height: 40px;
+        border: none;
+        outline: none;
+        border-radius: 40px;
+        cursor: pointer;
+        font-size: 1em;
+        color: #ffffff;
+        font-weight: 500;
+      }
+
+      .button2 {
+            position: absolute;
+            top: 5px;
+            right: 5px;
+            width: 35px;
+            height: 35px;
+            background: #d32f2f;
+            border: 2px solid #b71c1c;
+            border-radius: 5px;
+            cursor: pointer;
+            transition: transform 0.2s, background 0.3s;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            }
+
+       
+
+        .button2:hover {
+            background: #b71c1c;
+            transform: scale(1.1);
+            }
+
+        .button2:active {
+            transform: scale(0.9);
+            background: #880e4f;
             }
       `;
   
@@ -370,56 +523,148 @@ class EquiposCardAdmin extends HTMLElement {
   
       equipos.forEach(agregarTarjeta);
       shadow.appendChild(style);
-      shadow.appendChild(container)
-      // Crear barra de búsqueda
-  const searchBox = document.createElement("div");
-  searchBox.classList.add("input-box1");
-  searchBox.innerHTML = `
-    <div class="input-box2">
-      <input id="searchInput" type="text" required placeholder=" ">
-      <label for="searchInput"><box-icon name='search' color='#ffffff' ></box-icon>Buscar equipo...</label>
-    </div>
-  `;
-      shadow.appendChild(searchBox);
-      shadow.appendChild(container);
-      const searchInput = shadow.getElementById("searchInput");
-  
-  searchInput.addEventListener("input", () => {
-    const value = searchInput.value.toLowerCase();
-  
-    // Limpiar contenedor antes de renderizar resultados filtrados
-    container.innerHTML = "";
-  
-    const filteredPilotos = equipos.filter(p =>
-      p.nombre.toLowerCase().includes(value) ||
-      p.pais.toLowerCase().includes(value)
-    );
-  
-    filteredPilotos.forEach(agregarTarjeta);
+      const buttonAdd = document.createElement('button');
+    buttonAdd.setAttribute('data-modal-target', '#modal');
+    buttonAdd.id = 'buttonAdd';
+    buttonAdd.textContent = 'Agregar Piloto';
+
+    const overlay = document.createElement('div');
+    overlay.id = "overlay";
+
+    const formAdd = document.createElement('form');
+    formAdd.classList.add("modal");
+    formAdd.id = 'modal';
+    formAdd.innerHTML = `
+      <div class="modal-header">
+        <h2>Nuevo Equipo</h2>
+        <button data-close-button class="close-button">&times;</button>
+      </div>
+      <div class="modal-body">
+        <div class="input-box"><input id="name" type="text" required><label>Nombre</label></div>
+        <div class="input-box"><input type="text" required id="country"><label>Pais</label></div>
+        <div class="input-box"><input type="text" required id="motor"><label>Motor</label></div>
+        <div class="input-box"><input type="text" required id="pilots"><label>Pilotos (coma separadas)</label></div>
+        <div class="input-box"><input type="text" required id="image"><label>Url Imagen</label></div>
+        <button type="submit" class="button">Agregar</button>
+      </div>
+    `;
+
+    
+
+    // Crear barra de búsqueda
+const searchBox = document.createElement("div");
+searchBox.classList.add("input-box1");
+searchBox.innerHTML = `
+  <div class="input-box2">
+    <input id="searchInput" type="text" required placeholder=" ">
+    <label for="searchInput"><box-icon name='search' color='#ffffff' ></box-icon>Buscar equipo...</label>
+  </div>
+`;
+shadow.appendChild(searchBox);
+    shadow.appendChild(overlay);
+    shadow.appendChild(formAdd);
+    shadow.appendChild(container);
+    shadow.appendChild(buttonAdd);
+    const searchInput = shadow.getElementById("searchInput");
+
+searchInput.addEventListener("input", () => {
+  const value = searchInput.value.toLowerCase();
+
+  // Limpiar contenedor antes de renderizar resultados filtrados
+  container.innerHTML = "";
+
+  const filteredteams = equipos.filter(p =>
+    p.nombre.toLowerCase().includes(value) ||
+    p.pais.toLowerCase().includes(value)
+  );
+
+  filteredteams.forEach(agregarTarjeta);
+});
+const teamsLinks = document.querySelectorAll('.teams-links');
+const teamsSection = document.getElementById('teams-section');
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+
+
+    if ([...teamsLinks].includes(link)) {
+        teamsSection.style.display = 'block';
+    } else {
+        teamsSection.style.display = 'none';
+    }
+
+
+    if (window.innerWidth <= 768) {
+      toggleMenu();
+    }
   });
-      
-  const teamsLinks = document.querySelectorAll('.teams-links');
-  const teamsSection = document.getElementById('teams-section');
-  const navLinks = document.querySelectorAll('nav a');
-  
-  navLinks.forEach(link => {
-    link.addEventListener('click', (e) => {
-      e.preventDefault();
-  
-  
-      if ([...teamsLinks].includes(link)) {
-          teamsSection.style.display = 'block';
-      } else {
-          teamsSection.style.display = 'none';
-      }
-  
-  
-      if (window.innerWidth <= 768) {
-        toggleMenu();
-      }
+});
+
+    // Abrir y cerrar formulario de agregar piloto
+    const openModalButtons = shadow.querySelectorAll('[data-modal-target]');
+    const closeModalButtons = shadow.querySelectorAll('[data-close-button]');
+    const overlay1 = shadow.getElementById('overlay');
+
+    openModalButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        const modal = shadow.querySelector(button.dataset.modalTarget);
+        if (modal) {
+          modal.classList.add('active');
+          overlay1.classList.add('active');
+        }
+      });
     });
-  });
-    }}
+
+    closeModalButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        const modal = button.closest('.modal');
+        if (modal) {
+          modal.classList.remove('active');
+          overlay1.classList.remove('active');
+        }
+      });
+    });
+
+    // Manejo del formulario
+    const form = shadow.getElementById('modal');
+    if (form) {
+      form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const name = shadow.getElementById('name').value;
+        const country = shadow.getElementById('country').value;
+        const motor = shadow.getElementById('motor').value;
+        const pilots = shadow.getElementById('pilots').value;
+        const image = shadow.getElementById('image').value;
+
+        const newteam = {
+          nombre: name,
+          pais: country,
+          motor: motor,
+          pilotos: pilots.split(',').map(h => h.trim()),
+          url: image
+        };
+
+        equipos.push(newteam);
+        localStorage.setItem("equipo", JSON.stringify(equipos));
+        renderCard(newteam); // Renderizar nuevo piloto
+
+        // Cerrar modal
+        form.classList.remove('active');
+        overlay1.classList.remove('active');
+
+        // Limpiar campos
+        shadow.getElementById('name').value = "";
+        shadow.getElementById('country').value = "";
+        shadow.getElementById('motor').value = "";
+        shadow.getElementById('pilots').value = "";
+        shadow.getElementById('image').value = "";
+      });
+    }
+  }
+}
   
   customElements.define('equipos-card-admin', EquiposCardAdmin);
   
